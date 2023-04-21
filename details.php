b3034b00e5efc732f0a806f969fe4a910efc1298
eelli002 <elijah_elliott@hotmail.com>
2023-04-20 23:43:09 -0700

    Order Deletion

--- details.php
+++ details.php
@@ -1,14 +1,49 @@
 <?php
-    /* Retrieves Single order based on URL parameter, authenicates it,
-    and prints if valid else throws error */
-    include ('config/db_connect.php');
+    /* Checks to see if a POST request has been made to delete a pizza,
+    if it does then it passes the id to deletePizza */
+    if (isset($_POST['delete'])) {
+        $delete_id = $_POST['delete_id'];
+        deletePizza($delete_id);
+    }
+
+    /* Checks to see if we have passed in an arguement for id, captures it
+    from the URL and sends it to our getPizza function to retrieve from database */
     if (isset($_GET['id'])) {
-        $id = mysqli_real_escape_string($connection, $_GET['id']);
+        $id = $_GET['id'];
+        $pizza = getPizza($id);
+    }
+
+    /* Retrieves the pizza order from the database with the given ID and returns
+    it as an associative array. */
+    function getPizza($id) {
+        include ('config/db_connect.php');
+        $id = mysqli_real_escape_string($connection, $id);
         $selectPizzaQuery = "SELECT * FROM pizzaOrders WHERE id = $id";
         $result = mysqli_query($connection, $selectPizzaQuery);
+        if (!$result) {
+            echo 'Error executing query: ' . mysqli_error($connection);
+            exit();
+        }
         $pizza = mysqli_fetch_assoc($result);
         mysqli_free_result($result);
         mysqli_close($connection);
+        return $pizza;
+    }
+
+    /* Deletes the pizza order with the given ID from the database, if an error
+    occurs we alert the user else redirect them back to the index page. */
+    function deletePizza($id) {
+        include ('config/db_connect.php');
+        $id = mysqli_real_escape_string($connection, $id);
+        $deleteQuery = "DELETE FROM pizzaOrders WHERE id = $id";
+        $result = mysqli_query($connection, $deleteQuery);
+        if (!$result) {
+            echo 'Error executing delete query: ' . mysqli_error($connection);
+            exit();
+        }
+        else {
+            header('Location: index.php');
+        }
     }
 ?>
 
@@ -23,6 +58,12 @@
                 <p>On: <?php echo date($pizza['created_at']); ?></p>
                 <h5>Toppings:</h5>
                 <p><?php echo htmlspecialchars($pizza['toppings']); ?></p>
+
+                <form action="details.php" method="POST">
+                    <input type="hidden" name="delete_id" value="<?php echo $pizza['id']; ?>" >
+                    <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+                </form>
+
             <?php else: ?>
                 <h5>This is not a valid order number</h5>
             <?php endif; ?>
